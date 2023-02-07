b4bb9e1e1fb0d3adf36dea2c6eb8c1e353548559
luvlqq <lualgod92@gmail.com>
2023-02-08 01:45:07 +0300

    Connect module Connected module to mongoose

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ProductController } from './product.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthModule } from '../auth/auth.module';
+import { ProductModel } from './product.model';
 
 @Module({
   controllers: [ProductController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ProductModel,
+        schemaOptions: {
+          collection: 'Product',
+        },
+      },
+    ]),
+  ],
 })
 export class ProductModule {}
--- src/top-page/top-page.model.ts
+++ src/top-page/top-page.model.ts
@@ -1,3 +1,6 @@
+import { prop } from '@typegoose/typegoose';
+import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
+
 export enum TopLevelCategory {
   Course,
   Services,
@@ -5,23 +8,44 @@ export enum TopLevelCategory {
   Products,
 }
 
-export class TopPageModel {
-  _id: string;
-  firstLevelCategory: TopLevelCategory;
-  secondCategory: string;
+export class HhData {
+  @prop()
+  count: number;
+  @prop()
+  juniorSalary: number;
+  @prop()
+  middleSalary: number;
+  @prop()
+  seniorSalary: number;
+}
+
+export class TopPageAdvantage {
+  @prop()
   title: string;
+  @prop()
+  description: string;
+}
+export interface TopPageModel extends Base {}
+export class TopPageModel extends TimeStamps {
+  @prop({ enum: TopLevelCategory })
+  firstLevelCategory: TopLevelCategory;
+  @prop()
+  secondCategory: string;
+  @prop({ unique: true })
+  alias: string;
+  @prop()
+  title: string;
+  @prop()
   category: string;
-  hh?: {
-    count: number;
-    juniorSalary: number;
-    middleSalary: number;
-    seniorSalary: number;
-  };
-  advanteges: {
-    title: string;
-    description: string;
-  }[];
+
+  @prop({type: () => HhData})
+  hh?: HhData;
+  @prop({ type: () => [TopPageAdvantage] })
+  advanteges: TopPageAdvantage[];
+  @prop()
   seoText: string;
+  @prop()
   textTitle: string;
+  @prop({ type: () => [String] })
   tags: string[];
 }
