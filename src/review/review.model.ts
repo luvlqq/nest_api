db4f71a2e74b66fe5ab7bcd0e8e917f19c5a3fad
luvlqq <lualgod92@gmail.com>
2023-02-08 02:15:55 +0300

    review service create a review service, models for db

--- src/review/review.model.ts
+++ src/review/review.model.ts
@@ -1,5 +1,6 @@
 import { Base, TimeStamps } from '@typegoose/typegoose/lib/defaultClasses';
 import { prop } from "@typegoose/typegoose";
+import { Types } from 'mongoose';
 
 export interface ReviewModel extends Base {}
 export class ReviewModel extends TimeStamps {
@@ -11,6 +12,7 @@ export class ReviewModel extends TimeStamps {
   description: string;
   @prop()
   rating: number;
+
   @prop()
-  createdAt: Date;
+  productId: Types.ObjectId;
 }
