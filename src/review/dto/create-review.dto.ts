aabff040570f855144b98e877eed9d7ed9208a48
luvlqq <lualgod92@gmail.com>
2023-02-08 03:18:09 +0300

    pipes

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,21 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+  @IsString()
   name: string;
+  @IsString()
   title: string;
+  @IsString()
   description: string;
+
+  // TODO min class-validator
+  @Max(5)
+  @Min(1)
+  @IsNumber()
   rating: number;
+  @IsString()
   productId: string;
+}
+function Min(arg0: number) {
+  throw new Error('Function not implemented.');
 }
